6a9b19f540dd722ca5af79aaac7a8ae2c4048743
phelsdon <phelsdon@previon.ch>
2023-07-16 19:07:56 +0200

    add image upload in frontend and backend; add check for rating required

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -2,7 +2,7 @@
   <div v-if="submitSuccess" @click="modalDismiss" class="modal flex flex-col justify-center w-full ">
     <h1 class="text-8xl text-amber-50 block ">Submission Successful</h1>
   </div>
-  <form class="max-w-2xl bg-amber-100 h-96 mx-auto p-8 my-8 flex flex-col justify-around items-start rounded-2xl">
+  <form @submit.prevent="" class="max-w-2xl bg-amber-100 mx-auto p-8 my-8 flex flex-col justify-around items-start rounded-2xl">
     <div class="formControl" >
       <label>Title</label>
       <div class="validation">
@@ -14,18 +14,21 @@
       <label>Author</label>
       <div class="validation">
         <p v-if="validationErrors.author">{{validationErrors.author[0]}}</p>
-        <input v-model="author" type="email" />
+        <input v-model="author" type="text" />
       </div>
     </div>
     <div class="formControl">
       <label>Rating</label>
-      <select v-model="rating">
-        <option value="1" >1</option>
-        <option value="2" >2</option>
-        <option value="3" >3</option>
-        <option value="4" >4</option>
-        <option value="5" >5</option>
-      </select>
+      <div class="validation">
+        <p v-if="validationErrors.title">{{validationErrors.rating}}</p>
+        <select v-model="rating">
+          <option value="1" >1</option>
+          <option value="2" >2</option>
+          <option value="3" >3</option>
+          <option value="4" >4</option>
+          <option value="5" >5</option>
+        </select>
+      </div>
     </div>
     <div class="formControl">
       <label>Bestseller</label>
@@ -40,13 +43,20 @@
         </div>
       </div>
     </div>
-  </form>
+    <div class="formControl">
+      <label>Picture Upload</label>
+      <div class="validation">
+        <div v-if="fileUploadErrors.length > 0">
+          <p v-for="(error, idx) in fileUploadErrors" :key="idx"><span>{{error}}</span></p>
+        </div>
+        <input type="file" @change="handleFileUpload" />
+      </div>
+    </div>
     <div class="formControl submitFC">
       <button @click="handleSubmitForm" class="block bg-white py-2 px-4 rounded-md">Submit</button>
       <button class="block bg-white py-2 px-4 rounded-md">Cancel</button>
     </div>
-
-
+  </form>
   <p>Title: {{title}} | Author: {{author}} | Rating: {{rating}} | Bestseller: {{bestseller}}</p>
   <div class="errorMessages">
     <ul>
@@ -58,6 +68,7 @@
 </template>
 
 <script setup>
+  // import axios from "axios";
   import {ref} from 'vue'
 
   const title = ref('')
@@ -65,44 +76,81 @@
   const rating = ref(null)
   const bestseller = ref('')
   const validationErrors = ref({
-    title: [], author: []
+    title: [], author: [], rating: ''
   })
   const submitSuccess = ref(false) // show submit modal
+  const imageFile = ref({})
+  const allowedFileSize = ref(8) // in Megabytes
+  const fileUploadErrors = ref([])
   const modalDismiss = () => {
     submitSuccess.value = false
   }
+  const handleFileUpload = (e) => {
+    fileUploadErrors.value = [] // reset for each submission attempt
+    if (!e.target.files[0]) return
+    const file = e.target.files[0]
+
+    if (Math.round(file.size / 1024 / 1024) < allowedFileSize.value) {
+      let fileSize = Math.round(file.size / 1024 / 1024)
+    } // allowed file size
+    else fileUploadErrors.value.push('file upload size exceeded')
+
+    let is_ValidFileType = ['jpg', 'jpeg', 'png', 'gif'].includes(file.name.toLowerCase().split('.').pop())
+    if (!is_ValidFileType) fileUploadErrors.value.push('invalid file type')
+
+    // if there are errors, display and return
+    if (fileUploadErrors.value.length > 0) {
+      // console.log('file upload errors: ',fileUploadErrors.value)
+      return
+    }
+    imageFile.value = file // transfer to ref
+  }
   const handleSubmitForm = async () => {
+    if (!rating.value) {
+      validationErrors.value.rating = 'Please Enter a Rating'
+      console.log('a rating is required')
+      return
+    }
     let is_bestselling
     bestseller.value == 'Yes'? is_bestselling = true : is_bestselling = false
 
-    const data = {
-      title: title.value,
-      author: author.value,
-      rating: rating.value,
-      is_bestselling
-    }
-    console.log('data before sending: ', data)
+    const formData = new FormData()
+    formData.append('title', title.value)
+    formData.append('author', author.value)
+    formData.append('is_bestselling', is_bestselling)
+    formData.append('rating', rating.value)
+    formData.append('file', imageFile.value)
+
     try {
       const response = await fetch('http://127.0.0.1:8000/books/api', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+        body: formData
+        // headers: {
+        //   'Content-Type': 'application/json', // not with formData, just with normal data obj
+        // },
+        // body: JSON.stringify(data),
       })
-
+    //   AXIOS in case somebody wants a progress bar. But the goddam 400 return without messages is wasting my time
+    //   const response = await axios('http://127.0.0.1:8000/books/api', {
+    //     method:'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     data: data,
+    //   })
+    //   console.log('fuck axios')
+    //   if (response.error) console.log("reponse.error ", response.error)
       if (!response.ok) {
-        const postResponse = await response.json()
-        // console.log('error responses from server: ', postResponse)
-        validationErrors.value = postResponse
+        const postResponse = await response.json() // get the error messages from django
+        validationErrors.value = postResponse // populate and activate the errors in template
+        console.log(validationErrors.value)
         throw Error('404: failed to post data')
       }
       // no errors from server
       submitSuccess.value = true
-      validationErrors.value = {title: [], author: []} // reset otherwise error in template
+      validationErrors.value = {title: [], author: [], rating: []} // reset otherwise error in template
     } catch(err) {
       console.log(err.message)
-
     }
   }
 
@@ -115,7 +163,6 @@
   label {
     margin-right: 2em;
   }
-
   input {
     padding: .5em;
     border-radius: .5em;
@@ -131,6 +178,7 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: 1.5em 0
   }
   .radioFC {
     width: 60%;
@@ -161,7 +209,5 @@
     top: 0;
     cursor: pointer;
   }
-
-
 
 </style>
